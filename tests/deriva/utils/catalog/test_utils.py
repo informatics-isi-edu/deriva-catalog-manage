48d8ff6aa104e76731d9b0ba7b95bdfaa6d85632
mikedarcy <mikedarcy@users.noreply.github.com>
2019-06-12 11:29:51 -0700

    Fix some unit test issues and remove attrdict from install_requires.

--- tests/deriva/utils/catalog/test_utils.py
+++ tests/deriva/utils/catalog/test_utils.py
@@ -159,5 +159,5 @@ def create_catalog(server):
     logger.info('Catalog_id is {}'.format(catalog_id))
 
     catalog = DerivaCatalog(server, catalog_id=catalog_id, validate=False)
-    logger.critical('Time to create catalog %s', time.time()-t0)
+    logger.info('Time to create catalog %s', time.time()-t0)
     return catalog
